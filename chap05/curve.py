bff608f6546847ee1f09800d82ce7c32fcc51c15
Allison Patterson <allison.patterson@students.olin.edu>
2013-10-02 12:17:02 -0400

    Add working curve.py (dragon curve).

--- chap05/curve.py
+++ chap05/curve.py
@@ -1,12 +1,28 @@
+from swampy.TurtleWorld import *
+world = TurtleWorld()
+bob = Turtle()
+bob.delay = 0.00001
+
 def dragon(t, l, n):
 	x = l*(0.5)**(0.5)
 	if n == 0 or x < 1:
 		fd(t, l)
 		return
-	dragon(t, x, n-1)
-	lt(t, 90)
-	dragon(t, x, n-1)
+
+	dragon(t,x,n-1)
+	lt(t,90)
+	funky(t,x,n-1)
+
+def funky(t, l, n):
+	x = l*(0.5)**(0.5)
+	if n == 0 or x < 1:
+		fd(t, l)
+		return
+
+	dragon(t,x,n-1)
+	lt(t,-90)
+	funky(t,x,n-1)
 
 
-dragon(bob, 180, 2)
+dragon(bob, 180, 12)
 wait_for_user()
