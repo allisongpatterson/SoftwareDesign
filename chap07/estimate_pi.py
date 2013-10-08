251a99f5cde296100c96a5dd119ba8e1deb49f9b
Allison Patterson <allison.patterson@students.olin.edu>
2013-10-08 19:13:18 -0400

    Add working estimate_pi().

---
+++ chap07/estimate_pi.py
@@ -0,0 +1,13 @@
+from math import *
+def estimate_pi():
+	k = 0
+	total = 0
+	coeff = (2*sqrt(2))/9801
+	while True:
+		factor = coeff * ((factorial(4*k)*(1103+26390*k)) / (factorial(k)**(4)*369**(4*k)))
+		if factor < 1e-15:
+			break
+		total = total + factor
+		k = k + 1
+		return (total)**(-1)
+print estimate_pi()
