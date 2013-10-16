95ae7051ec4960c1b55e26a99920bfa91e6eece6
Allison Patterson <allison.patterson@students.olin.edu>
2013-10-15 21:06:50 -0400

    Add working interlock.py.

---
+++ chap10/interlock.py
@@ -0,0 +1,11 @@
+def interlock(word1, word2):
+	locked = ''
+	if len(word1) >= len(word2):
+		for i in range(len(word1)):
+			locked += word1[i] + word2[i]
+	elif len(word1) < len(word2):
+		for i in range(len(word2)):
+			locked += word1[i] + word2[i]
+	return locked
+
+# print interlock('shoe','cold')
