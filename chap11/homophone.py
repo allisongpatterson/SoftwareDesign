db715ad9d1363f4e8bb5b7b0762c664d5d70aee0
Allison Patterson <allison.patterson@students.olin.edu>
2013-10-24 19:35:51 -0400

    Add homophone.py.

--- chap11/homophone.py
+++ chap11/homophone.py
@@ -1,3 +1,16 @@
-#I need help on this one. 
-#I plan on seeking that help tomorrow (10/23/13).
-#Sorry for any inconvenience.
+#Trouble downloading the pronouncing dictionary.
+#I'm going to pretend it works anyway.
+def is_homophone(word1, word2, phonetic):
+	if word1 or word2 not in phonetic:
+		return False
+	return phonetic[word1] == phonetic[word2]
+
+def sovlve_homophone_puzzle(word_list, phonetic):
+	for word in word_list:
+		word_new = word[1:]
+		if word_new in word_list:
+			if is_homophone(word, word_new) == True
+				new_word_new = word[0] + word[2:]
+				if new_word_new in word_list:
+					if is_homophone(word, new_word_new) == True:
+						return word, word_new, new_word_new
