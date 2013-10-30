b639a36b8b50985aece4bdb99bc23f03138cd494
Allison Patterson <allison.patterson@students.olin.edu>
2013-10-30 14:57:45 -0400

    Fixed flower.py and mypolygon.py (Sorry NINJA's).

--- chap04/flower.py
+++ chap04/flower.py
@@ -4,16 +4,24 @@ bob = Turtle()
 bob.delay = 0.0001
 
 def flower(t, n, r):
+	"""Draws a flower with n petals.
+	t: Turtle
+	n: positive, even integer
+	r: radius
+	"""
 	for i in range(n):
 		arc(t, r, angle = 360.0/n)
 		lt(t, ((n/2) - 1) * (360.0/n))
 		arc(t, r, angle = 360.0/n)
 		lt(t, (n/2) * (360.0/n))
 
-def daisy(t, n):
-	for i in range(n):
+def daisy(t):
+	"""Draws a sloppy daisy (just for funsies).
+	t: Turtle
+	"""
+	for i in range(6):
 		angle = 60
-		for i in range(n):
+		for i in range(6):
 			angle_new = angle + 120
 			angle_current = angle_new + 60
 		arc(t, 50, angle_current)
@@ -21,6 +29,7 @@ def daisy(t, n):
 	lt(t)
 	circle(t, 85)
 
-#daisy(bob, 6)
-flower(bob, 12, 180)
-wait_for_user()
+if __name__ == '__main__':
+	#daisy(bob)
+	flower(bob, 12, 180)
+	wait_for_user()
--- chap04/mypolygon.py
+++ chap04/mypolygon.py
@@ -1,37 +1,37 @@
 from swampy.TurtleWorld import *
 
-#world = TurtleWorld()
-#bob = Turtle()
-#bob.delay = 0.01
-#ray = Turtle()
+world = TurtleWorld()
+bob = Turtle()
+bob.delay = 0.01
 
 def square(t, l):
+	"""Draws square with side-length l.
+	"""
 	for i in range(4):
 		fd(t, l)
 		lt(t)
 	print t
 
-#square(bob, 100)
-#square(ray, 25)
-
 def polygon(t, n, l):
+	"""Draws n-sided polygon with side-length l.
+	"""
 	for i in range(n):
 		angle = 360.0/n
 		fd(t, l)
 		lt(t, angle)
 	print t
 
-#polygon(bob, 6, 60)
-
 def circle(t, r):
+	"""Draws circle with radius r.
+	"""
 	circumference = 2 * 3.14159 * r
 	n = int(circumference/3) + 1
 	l = circumference/n
 	polygon(t, n, l)
 
-#circle(bob, 50)
-
 def arc(t, r, angle):
+	"""Draws angle degrees of circle with radius r.
+	"""
 	arc_length = (2 * 3.14159 * r * angle)/360
 	n = int(arc_length/3) + 1
 	step_length = arc_length/n
@@ -42,6 +42,9 @@ def arc(t, r, angle):
 		lt(t, step_angle)
 	print t
 
-#arc(bob, 75, 270)
-
-#wait_for_user()
+if __name__ == '__main__':
+	square(bob, 100)
+	polygon(bob, 6, 70)
+	circle(bob, 50)
+	arc(bob, 75, 270)
+	wait_for_user()
