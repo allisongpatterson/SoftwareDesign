3a9a686500c4de9a980ff87610a65114beb05bb4
Allison Patterson <allison.patterson@students.olin.edu>
2013-10-30 14:37:12 -0400

    Fixed grid.py (Sorry NINJA's)

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,38 +1,19 @@
-def little_grid():
-	print '+ - - - - + - - - - +'
-	print '|         |         |'
-	print '|         |         |'
-	print '|         |         |'
-	print '|         |         |'
-	print '+ - - - - + - - - - +'
-	print '|         |         |'
-	print '|         |         |'
-	print '|         |         |'
-	print '|         |         |'
-	print '+ - - - - + - - - - +'
+def top_line(n):
+	"""Draws top line of grid.py.
+	n: positive integer
+	"""
+	print '+',
+	print '- - - - + ' * n
 
-def big_grid():
-	print '+ - - - - + - - - - + - - - - + - - - - +'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '+ - - - - + - - - - + - - - - + - - - - +'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '+ - - - - + - - - - + - - - - + - - - - +'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '+ - - - - + - - - - + - - - - + - - - - +'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '|         |         |         |         |'
-	print '+ - - - - + - - - - + - - - - + - - - - +'
+def grid(n):
+	"""Draws n by n grid.
+	n: positive integer
+	"""
+	for i in range(n):
+		top_line(n)
+		for i in range(4):
+			print '|         ' * (n+1)
+	top_line(n)
 
-big_grid()
-little_grid()
+if __name__ == '__main__':
+	grid(2)
